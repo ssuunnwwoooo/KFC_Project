3482ecc70dea29b400bd0eb9f2c9c9516af2ebf6
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-06-15 18:05:54 +0900

    Add files via upload

--- js/kfc.js
+++ js/kfc.js
@@ -28,5 +28,23 @@ $(function () {
     $('.mainContent .arrows .right').on('click', function () {
         $('.content_Slide').slick('slickNext')
     });
+
+    $('.history_menu li').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).index();
+        $('.history_content li').eq(idx).addClass('on').siblings().removeClass('on');
+    });
+
+
+
+    $('.history_menu .li .strong').on('click', function () {
+        $(this).addClass('on').siblings().removeClass('on')
+    });
+    $('.history_menu .li .span').on('click', function () {
+        $(this).addClass('on').siblings().removeClass('on')
+    });
+
+
+
 })
 
