c25399662ddbe4992d38c4555e6ad66774d20233
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-06-16 14:30:20 +0900

    Add files via upload

--- js/kfc.js
+++ js/kfc.js
@@ -1,5 +1,13 @@
 $(function () {
 
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
+
     $('.mainSlide').slick({
         arrows: false,
         dots: true,
